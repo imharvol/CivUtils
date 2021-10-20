f86bef21388a303300bf4c529dba986ec2622c14
imharvol <imharvol@gmail.com>
2021-10-20 09:57:14 +0200

    Add goDown method

--- civ-utils.js
+++ civ-utils.js
@@ -27,7 +27,6 @@ function inject (bot, option) {
 
     targetY = Math.floor(targetY)
     const initialY = Math.floor(bot.entity.position.y)
-
     if (targetY < initialY) throw new Error('The bot is above the targetY')
 
     for (let y = initialY; y < targetY;) { // y represents where we want to place the next block
@@ -36,7 +35,7 @@ function inject (bot, option) {
 
       // Check if we are above the block where we want to place the block
       if (Math.floor(bot.entity.position.y) >= y + 1) {
-        const referenceBlockPos = vec3(bot.entity.position.x, y - 1, bot.entity.position.z)
+        const referenceBlockPos = vec3(bot.entity.position.x, y - 1, bot.entity.position.z).floored()
         const referenceBlock = bot.blockAt(referenceBlockPos)
 
         // Equip a building block
@@ -50,12 +49,38 @@ function inject (bot, option) {
         try { // Sometimes placeBlock does fail, maybe because of delay? so we might have to try multiple times
           await bot.placeBlock(referenceBlock, vec3(0, 1, 0))
           y++
-        } catch (e) {}
+        } catch (err) {}
       } else {
         await once(bot, 'physicsTick')
       }
     }
     bot.setControlState('jump', false)
+  }
+
+  bot.civUtils.goDown = async (targetY, tools = []) => {
+    targetY = Math.floor(targetY)
+    const initialY = Math.floor(bot.entity.position.y)
+    if (targetY > initialY) throw new Error('The bot is below the targetY')
+
+    for (let y = initialY; y > targetY;) {
+      const blockPos = vec3(bot.entity.position.x, y - 1, bot.entity.position.z).floored()
+      const block = bot.blockAt(blockPos)
+
+      for (const tool of tools) {
+        if (bot.inventory.count(tool) > 0) {
+          await bot.equip(tool, 'hand')
+          break
+        }
+      }
+
+      try {
+        await bot.dig(block)
+        y--
+      } catch (err) {}
+
+      await once(bot, `blockUpdate:${blockPos}`)
+      await bot.civUtils.sleep(200) // For some reason if you don't wait a bit, it takes much longer to dig the block
+    }
   }
 }
 
