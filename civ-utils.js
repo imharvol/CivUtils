7d395b59a280f5158c058a136a6fc9b356c6f31e
imharvol <imharvol@gmail.com>
2021-10-20 09:22:43 +0200

    Add method goUp

--- civ-utils.js
+++ civ-utils.js
@@ -1,3 +1,6 @@
+const { once } = require('events')
+const vec3 = require('vec3')
+
 function inject (bot, option) {
   bot.civUtils = {}
 
@@ -13,10 +16,46 @@ function inject (bot, option) {
       await bot.lookAt(targetPos)
       bot.setControlState('forward', true)
 
-      await bot.civUtils.sleep(20)
+      await once(bot, 'physicsTick')
     }
 
     bot.setControlState('forward', false)
+  }
+
+  bot.civUtils.goUp = async (targetY, buildingBlocks = []) => {
+    if (buildingBlocks.length === 0) throw new Error('goUp should receive at least one building block')
+
+    targetY = Math.floor(targetY)
+    const initialY = Math.floor(bot.entity.position.y)
+
+    if (targetY < initialY) throw new Error('The bot is above the targetY')
+
+    for (let y = initialY; y < targetY;) { // y represents where we want to place the next block
+      await bot.look(bot.entity.yaw, -90) // Look down
+      bot.setControlState('jump', true) // Jump continuously
+
+      // Check if we are above the block where we want to place the block
+      if (Math.floor(bot.entity.position.y) >= y + 1) {
+        const referenceBlockPos = vec3(bot.entity.position.x, y - 1, bot.entity.position.z)
+        const referenceBlock = bot.blockAt(referenceBlockPos)
+
+        // Equip a building block
+        for (const buildingBlock of buildingBlocks) {
+          if (bot.inventory.count(buildingBlock) > 0) {
+            await bot.equip(buildingBlock, 'hand')
+            break
+          }
+        }
+
+        try { // Sometimes placeBlock does fail, maybe because of delay? so we might have to try multiple times
+          await bot.placeBlock(referenceBlock, vec3(0, 1, 0))
+          y++
+        } catch (e) {}
+      } else {
+        await once(bot, 'physicsTick')
+      }
+    }
+    bot.setControlState('jump', false)
   }
 }
 
